6f72df718fa9e029506a17074260d57470e1b5ef
HtetWaiYanAung74 <htetwyag75@gmail.com>
2025-04-28 17:59:06 +0700

    add oAuth2Client

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.json());
 const usersTokens = {};
 
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
+
 // Start OAuth login
 app.get('/auth', (req, res) => {
   const authUrl = oAuth2Client.generateAuthUrl({
